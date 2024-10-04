e7a524681a16b7c8ec9cb5c513727cdaa94e1b97
Dattatraygiri79@gmail.com <Dattatraygiri79@gmail.com>
2024-10-04 13:17:47 +0000

    done

--- models/example/table_creation.sql
+++ models/example/table_creation.sql
@@ -1,4 +1,4 @@
- create or replace temp table DBT.USERS.TRANS(id int,name varchar(255),salary float);
+ create or replace  table DBT.USERS.TRANS(id int,name varchar(255),salary float);
     insert into DBT.USERS.TRANS(id,name,salary) select * from DBT.PUBLIC.dbtuse;
 
     select * from DBT.USERS.TRANS
