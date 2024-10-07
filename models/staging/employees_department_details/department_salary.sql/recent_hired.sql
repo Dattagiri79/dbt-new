ff1fa0c9e72fc73351e3bf84a6d3f95e28e6f5c2
Dattatray Giri <Dattatraygiri79@gmail.com>
2024-10-07 11:48:55 +0000

    go

--- models/staging/employees_department_details/department_salary.sql/recent_hired.sql
+++ models/staging/employees_department_details/department_salary.sql/recent_hired.sql
@@ -8,7 +8,7 @@ with recent_hires AS (
         e.hire_date,
         d.department_name
     FROM {{ ref('norm_dbt') }} e
-    JOIN Departments d ON e.department_id = d.department_id
+    JOIN {{ source("DBT_DATTAGIRI79", "DEPARTMENTS") }} d ON e.department_id = d.department_id
     WHERE e.hire_date >= DATEADD(YEAR, -2, CURRENT_DATE)
 )
 select * from recent_hires
